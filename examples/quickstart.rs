13a789f713df1e91c03078a07a2eda5559d2508a
Kittenlover229 <artur.roos.artur@gmail.com>
2023-08-12 17:24:28 +0300

    Prefer constant instead of layers

--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -1,11 +1,14 @@
 use std::convert::Infallible;
 
-use wffle::{FrameBuilder, SpriteInstance, StandaloneRenderer};
+use wffle::{SpriteInstance, StandaloneRenderer};
+
+const BACKGROUND: &str = "background";
+const FOREGROUND: &str = "foreground";
 
 fn main() {
     let mut renderer = StandaloneRenderer::new("Wffle Quickstart Example");
-    renderer.set_layer("background", 0);
-    renderer.set_layer("foreground", 5);
+    renderer.set_layer(BACKGROUND, 0);
+    renderer.set_layer(FOREGROUND, 5);
 
     let texture_16x16 = image::load_from_memory(include_bytes!("16x16.png")).unwrap();
     let texture_8x16 = image::load_from_memory(include_bytes!("8x16.png")).unwrap();
@@ -19,53 +22,53 @@ fn main() {
         .finalize_and_repack();
 
     renderer
-        .run(move |renderer, input| {
+        .run::<Infallible>(move |renderer, input| {
             let cursor_pos = renderer.window_to_world(input.cursor_pos);
             let frame = renderer.begin_frame();
 
-            Ok::<FrameBuilder<'_>, Infallible>(
-                frame
-                    .draw_sprite_indexed(
-                        i8x8,
-                        "background",
-                        SpriteInstance {
-                            color: [0x00, 0xFF, 0xFF].into(),
-                            ..Default::default()
-                        },
-                    )
-                    .draw_sprite_indexed(
-                        i8x8,
-                        "foreground",
-                        SpriteInstance {
-                            position: cursor_pos,
-                            color: [0xFF, 0xFF, 0xFF].into(),
-                            ..Default::default()
-                        },
-                    )
-                    .draw_sprite_indexed(
-                        i16x16,
-                        "background",
-                        SpriteInstance {
-                            position: [0., 1.].into(),
-                            color: [0xFF, 0xFF, 0x00].into(),
-                            ..Default::default()
-                        },
-                    )
-                    .draw_sprite_indexed(
-                        i8x16,
-                        "background",
-                        SpriteInstance {
-                            position: [0., 2.].into(),
-                            opacity: 0.5,
-                            ..Default::default()
-                        },
-                    )
-                    .draw_egui(|ctx| {
-                        egui::Window::new("Demo Egui Window").show(ctx, |ui| {
-                            ui.label("Hello, world!");
-                        });
-                    }),
-            )
+            let frame = frame
+                .draw_sprite_indexed(
+                    i8x8,
+                    BACKGROUND,
+                    SpriteInstance {
+                        color: [0x00, 0xFF, 0xFF].into(),
+                        ..Default::default()
+                    },
+                )
+                .draw_sprite_indexed(
+                    i8x8,
+                    FOREGROUND,
+                    SpriteInstance {
+                        position: cursor_pos,
+                        color: [0xFF, 0xFF, 0xFF].into(),
+                        ..Default::default()
+                    },
+                )
+                .draw_sprite_indexed(
+                    i16x16,
+                    BACKGROUND,
+                    SpriteInstance {
+                        position: [0., 1.].into(),
+                        color: [0xFF, 0xFF, 0x00].into(),
+                        ..Default::default()
+                    },
+                )
+                .draw_sprite_indexed(
+                    i8x16,
+                    BACKGROUND,
+                    SpriteInstance {
+                        position: [0., 2.].into(),
+                        opacity: 0.5,
+                        ..Default::default()
+                    },
+                )
+                .draw_egui(|ctx| {
+                    egui::Window::new("Demo Egui Window").show(ctx, |ui| {
+                        ui.label("Hello, world!");
+                    });
+                });
+
+            Ok(frame)
         })
         .unwrap();
 }
